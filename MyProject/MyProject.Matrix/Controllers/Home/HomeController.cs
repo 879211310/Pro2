af013acd929fab445b7bd419ab33c06e36a92ae0
gezhanglong <38719428+gezhanglong@users.noreply.github.com>
2018-06-11 10:07:24 +0800

    2018061110
    2018061110

--- MyProject/MyProject.Matrix/Controllers/Home/HomeController.cs
+++ MyProject/MyProject.Matrix/Controllers/Home/HomeController.cs
@@ -6,6 +6,10 @@ using MyProject.Tasks;
 using MyProject.Services.Mappers;
 using MyProject.Task;
 using System;
+using MyProject.Matrix.Controllers.Account.ViewModels;
+using MyProject.Services.Utility;
+using MyProject.Core.Dtos;
+using MyProject.Core.Enum;
 
 namespace MyProject.Matrix.Controllers.Home
 {
@@ -13,7 +17,9 @@ namespace MyProject.Matrix.Controllers.Home
     { 
         private readonly AdminRoleMenuTask _roleMenuTask = new AdminRoleMenuTask();
         private readonly AdminMenuTask _menusTask = new AdminMenuTask();
+        private readonly AdminUserTask _adminUserTask = new AdminUserTask();
 
+        #region 主页面
         public ActionResult Index()
         {
             if (GetCurrentAdmin() == null)
@@ -36,8 +42,53 @@ namespace MyProject.Matrix.Controllers.Home
         }
 
         public ActionResult FirstIndex()
-        { 
+        {
             return View();
+        } 
+        #endregion 
+
+        #region 登录
+        public ActionResult LogInIndex()
+        {
+            var model = new LogOnModel();
+            return View(model);
         }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userPassword = _adminUserTask.GetByUserName(model.UserName);
+                if (userPassword == null)
+                    return AlertMsg("账号不存在", Request.UrlReferrer.PathAndQuery);
+                if (userPassword.Password != CryptTools.HashPassword(model.Password))
+                    return AlertMsg("账号或密码不正确", Request.UrlReferrer.PathAndQuery);
+                if (userPassword.IsLock)
+                    return AlertMsg("对不起，您的账号被锁定", Request.UrlReferrer.PathAndQuery);
+                //if (model.ValidationCode != Session["code"].ToString())
+                //    return AlertMsg("验证码不正确", Request.UrlReferrer.PathAndQuery);
+                AccountDto account = new AccountDto();
+                account.AdminUserId = userPassword.AdminUserId;
+                account.UserName = userPassword.UserName;
+                account.RoleId = userPassword.RoleId;
+                Session["Account"] = account;
+                SysLogTask.AddLog(new MyProject.Core.Entities.SysLogDto() { Message = "", Module = LogModuleEnum.Land, Type = LogTypeEnum.Land, Operator = userPassword.UserName, Result = "登陆成功" });
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+        #endregion
+
+        #region 退出
+        public ActionResult LogOut()
+        {
+            Session["Account"] = "";
+            return RedirectToAction("Index", "Account");
+        }
+        #endregion
+
+    
+
     }
 }
